c29d15b1129ae8fb495f88d232aa207b2258a5ad
Ali Zamir Sakhiezada <zamirsakhiezada@gmail.com>
2026-07-20 16:33:00 +0200

    feat: when a task is clicked a dialog opens which contents more information about the task

--- scripts/board.js
+++ scripts/board.js
@@ -22,6 +22,16 @@ function addTaskClose() {
     dialogRef.close();
 }
 
+function taskOpen() {
+    let dialogRef = document.getElementById("task");
+    dialogRef.showModal();
+}
+
+function taskClose() {
+    let dialogRef = document.getElementById("task");
+    dialogRef.close();
+}
+
 function toggleContactList() {
     document.getElementById("contact-list").classList.toggle("display-none");
     document.getElementById("contacts-arrow").classList.toggle("upside");
